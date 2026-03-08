60f5e535ff6415e4d834de2333f524d16687f24a
kaushiki-alt <kaushikibansal10e@gmail.com>
2026-03-08 19:21:36 +0530

    fix the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import { Button } from "@/components/ui/button";
-import AppAreaLayout from "./app/layout";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      home
+    <main className="flex items-center justify-center h-screen my-auto">
+
+      <Link href="/app/dashboard" className="text-center">
+        <h1 className="text-3xl font-bold underline hover:text-blue-900">Go to Dashboard</h1>
+      </Link>
     </main>
   );
 }
